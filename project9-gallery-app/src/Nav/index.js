f0fe4adaf55b8568ebc225f8952a2267541e3a59
Daniel Perez <pereznetworks@live.com>
2018-09-16 09:50:48 -0700

    fine tuning navlinks

--- project9-gallery-app/src/Nav/index.js
+++ project9-gallery-app/src/Nav/index.js
@@ -4,11 +4,11 @@ const Nav = (props) => {
 
   return (<nav className="main-nav">
     <ul>
-      <li><a href='/cats'>Cats</a>
+      <li><a href='/Hippopotamus'>Hippopotamus</a>
       </li>
-      <li><a href='/dogs'>Dogs</a>
+      <li><a href='/eagles'>Eagles</a>
       </li>
-      <li><a href='/computers'>Computers</a>
+      <li><a href='/horses'>Horses</a>
       </li>
       <li><a href='/search'>Search</a>
       </li>
