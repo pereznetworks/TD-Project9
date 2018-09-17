07a1ea43d248c64cf7eca286515c7b1b8aefa923
Daniel Perez <pereznetworks@live.com>
2018-09-17 09:00:57 -0700

    restoring Header/index.js

---
+++ project9-gallery-app/src/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Nav from '../Nav';
+import Gallery from '../Gallery'
+
+const Header = (props ) => {
+
+    if (props.match.params.query){
+      let query = props.query;
+      props.onSearch(query);
+    }
+
+
+    return (<div>
+      <h2>Flickr Photo Gallery</h2>
+      <Nav onSearch={props.onSearch}/>
+      <div className="photo-container">
+        <ul>
+          <Gallery {...props}
+              flickr={props.flickr}
+              photos={props.photos}
+          />
+        </ul>
+      </div>;
+    </div>);
+}
+
+export default Header
