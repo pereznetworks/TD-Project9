f88e69682d03e5fd49feb0324966591411133f76
Daniel Perez <34382720+pereznetworks@users.noreply.github.com>
2018-09-17 08:37:00 -0700

    deleting so not showing my api-keys

--- project9-gallery-app/src/Flickr/config.js
+++
@@ -1,9 +0,0 @@
-// my flickr api key and secret
-// not to be shared with anyone
-
-const Api = {
-    key: '04de892813384404960203eec707bdf7',
-    secret: 'b8956dad07128762',
-};
-
-export default Api;
